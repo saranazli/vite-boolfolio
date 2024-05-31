07c0b01a91f51e6d98bd95baa3ff955f861a0ca9
s4r44d43 <sararez@live.it>
2024-05-31 14:31:31 +0200

    [start] without api

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script>
+
+  // in attesa di fare il db del progetto laravel Auth per creare il model e quindi avere la mia apiurl, ho gettato le basi di quel che abbiamo ripassato in classe, ovviamente la api è errata.
+
+  import axios from 'axios';
+  import {store} from './data/store';
+
+  export default {
+
+    data(){
+      return{
+        posts: []
+      }
+    },
+
+    methods:{
+      getApi(){
+        axios.get(store.apiUrl)
+          .then(result =>{
+            this.posts = result.data
+          })
+        .catch(error =>{
+          console.log(error.message);
+        })
+      }
+    },
+    
+    mounted(){
+      this.getApi();
+    }
+  }
+
+
+</script>
+
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
